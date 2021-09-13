248d34f0bbdc2b431861d75b214402ae67a6d24a
Yulia <ulaboyko@ukr.net>
2021-09-13 11:15:57 +0300

    home work 11 color-switch

---
+++ src/js/color-switch.js
@@ -0,0 +1,24 @@
+import colors from '../db/colors.json';
+import refs from '../js/refs';
+
+refs.startBtn.addEventListener('click', startCangeColor);
+refs.stopBtn.addEventListener('click', stopCangeColor);
+
+let timerId;
+
+function startCangeColor() {
+  refs.startBtn.disabled = true;
+
+   timerId = setInterval(() => {
+    document.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+};
+
+function stopCangeColor() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+};
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
